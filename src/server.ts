06ae060faf6b9c330cc0ffd36e5c09fc4f948a06
Teerawut Saesim <55015430+ribbinpo@users.noreply.github.com>
2023-10-15 23:21:23 +0700

    Initial commit

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import express, { Request, Response } from 'express';
+import "dotenv/config";
+
+const app = express();
+const port = process.env.PORT;
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Express + TypeScript Server');
+});
+
+app.listen(port, () => {
+  console.log(`[server]: Server is running on ${port}`);
+});
