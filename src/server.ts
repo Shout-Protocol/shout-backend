5923fd78acb9351e62c77523f17a7d68e69577ba
ribbinpo <ribbinpo@gmail.com>
2023-10-16 10:24:50 +0700

    build: :building_construction: setup apollo-server configuration

--- src/server.ts
+++ src/server.ts
@@ -1,21 +1,37 @@
 import express, { Request, Response } from "express";
 import bodyParser from "body-parser";
+import { ApolloServer } from "apollo-server-express";
 
 import "dotenv/config";
 
+import typeDefs from "./models/typeDefs";
+import resolvers from "./resolvers/resolvers";
 import AuthRouter from "./routes/auth.route";
 
-const app = express();
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
 const port = process.env.PORT || "4000";
+const server = new ApolloServer({ typeDefs, resolvers });
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Server is running");
-});
+const startServer = async () => {
+  await server.start();
 
-app.use("/auth", AuthRouter);
+  const app = express();
 
-app.listen(port, () => {
-  console.log(`[server]: Server is running on ${port}`);
-});
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(bodyParser.json());
+
+  server.applyMiddleware({ app, path: "/graphql" });
+
+  app.use("/auth", AuthRouter);
+
+  app.get("/", (req: Request, res: Response) => {
+    res.send("Server is running");
+  });
+
+  app.listen(port, () => {
+    console.log(`[server]: Server is running on ${port}`);
+  });
+};
+
+startServer().catch((error) =>
+  console.log("Error starting the server: ", error)
+);
